dafc09868afaa61eee2ade66a6a6ad0b806e2ca3
manan <aoy.mananchaya@gmail.com>
2016-11-12 16:00:36 +0700

    Read Image Path finish

--- app/src/main/java/cskku/amornpalang/mananchaya/easykku/SignUpActivity.java
+++ app/src/main/java/cskku/amornpalang/mananchaya/easykku/SignUpActivity.java
@@ -1,6 +1,11 @@
 package cskku.amornpalang.mananchaya.easykku;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,17 +14,29 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import java.net.URL;
+
 public class SignUpActivity extends AppCompatActivity {
 
     //Explicit
-    private EditText nameEditText,phoneEditText,
+    private EditText nameEditText, phoneEditText,
             userEditText, passwordEditText;
     private ImageView imageView;
     private Button button;
 
-    private String nameString,phoneString,userString, passwordString;
-
-
+    private String nameString, phoneString, userString, passwordString,
+                    imagePathString, imageNameString;
+    private Uri uri;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
 
     @Override
@@ -35,6 +52,7 @@ public class SignUpActivity extends AppCompatActivity {
         imageView = (ImageView) findViewById(R.id.imageView);
         button = (Button) findViewById(R.id.button3);
 
+
         //SignUP Controller
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -47,10 +65,10 @@ public class SignUpActivity extends AppCompatActivity {
                 passwordString = passwordEditText.getText().toString().trim();
 
                 //Check Space
-                if (nameString.equals("") || phoneString.equals("")||
+                if (nameString.equals("") || phoneString.equals("") ||
                         userString.equals("") || passwordString.equals("")) {
                     //Have space
-                    Log.d("12novV1","Have Space");
+                    Log.d("12novV1", "Have Space");
                     MyAlert myAlert = new MyAlert(SignUpActivity.this, R.drawable.kon48,
                             "มีช่องว่าง", "กรุณากรอกให้ครบทุกช่อง");
                     myAlert.myDialog();
@@ -65,25 +83,94 @@ public class SignUpActivity extends AppCompatActivity {
                 //Intent ย้ายการทำงานจากอีกหน้าไป อีกหน้าหนึ่ง
                 Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
                 intent.setType("image/*"); //เปิดโปรแกรมทุกโปรแกรมที่สามารถเปิดดูรูปภาพได้
-                startActivityForResult(Intent.createChooser(intent,"โปรเลือกแอปดูภาพ"),0);
+                startActivityForResult(Intent.createChooser(intent, "โปรเลือกแอปดูภาพ"), 0);
                 // 0 ค่าของตัว request รูปภาพ
             } // onClick
         });
 
 
-
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }// Main Method
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if ((requestCode == 0)&&(resultCode == RESULT_OK)){
+        if ((requestCode == 0) && (resultCode == RESULT_OK)) {
             Log.d("12novV1", "Result OK");
+            //Show image
+            uri = data.getData();
+            try {
+                Bitmap bitmap = BitmapFactory.decodeStream(getContentResolver()
+                        .openInputStream(uri));
+                imageView.setImageBitmap(bitmap);
 
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+
+            //Find Paht of Image
+            imagePathString = myFindPath(uri);
+            Log.d("12novV1", "imagePath ==>" + imagePathString);
 
         } // if
 
 
     }// onActivity
+
+    private String myFindPath(Uri uri) {
+
+        String result = null;
+        String[] strings = {MediaStore.Images.Media.DATA};
+        Cursor cursor = getContentResolver().query(uri,strings,null,null,null);
+
+        if (cursor != null) {
+            cursor.moveToFirst();
+            int index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            result=cursor.getString(index);
+        } else {
+            result = uri.getPath();
+        }
+
+
+        return result;
+    }
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("SignUp Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
+    }
 } //Main class
