132744cd9d893066b1349abe5a7a5bf62bb4211a
manan <aoy.mananchaya@gmail.com>
2016-11-13 12:07:18 +0700

    get JSON one row  finish

--- app/src/main/java/cskku/amornpalang/mananchaya/easykku/MainActivity.java
+++ app/src/main/java/cskku/amornpalang/mananchaya/easykku/MainActivity.java
@@ -1,16 +1,30 @@
 package cskku.amornpalang.mananchaya.easykku;
 
+import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
 
 public class MainActivity extends AppCompatActivity {
 
     //Explicit
     //  Access | Type | Name
     private Button signInButton, signUpButton;
+    private EditText userEditText,passwordEditText;
+    private String userString, passwordString;
+    private MyConstant myConstant;
+
+
+
 
 
     @Override
@@ -18,9 +32,45 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        myConstant = new MyConstant();
+
         // Bind Widget
         signInButton = (Button) findViewById(R.id.button);
         signUpButton = (Button) findViewById(R.id.button2);
+        userEditText = (EditText) findViewById(R.id.editText5);
+        passwordEditText = (EditText) findViewById(R.id.editText6);
+
+        //SignIn Controller
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Get Value from Edit text
+                userString = userEditText.getText().toString().trim();
+                passwordString = passwordEditText.getText().toString().trim();
+
+                // Check Space
+                if (userString.equals("")) {
+                    //Have Space
+                    MyAlert myAlert = new MyAlert(MainActivity.this, R.drawable.doremon48,
+                            getResources().getString(R.string.title_UserFalse),
+                            getResources().getString(R.string.message_UserFalse));
+                    myAlert.myDialog();
+
+                } else if (passwordString.equals("")) {
+                    //Have Space
+                    MyAlert myAlert = new MyAlert(MainActivity.this, R.drawable.doremon48,
+                            getResources().getString(R.string.title_PassFalse),
+                            getResources().getString(R.string.message_PassFalse));
+                    myAlert.myDialog();
+                } else {
+                    //No Space
+                    SynUser synUser = new SynUser(MainActivity.this);
+                    synUser.execute(myConstant.getUrlGetJSON());
+                }
+
+            }// onClick
+        });
+
 
         //Sign up Controller
         signUpButton.setOnClickListener(new View.OnClickListener() {
@@ -33,5 +83,43 @@ public class MainActivity extends AppCompatActivity {
 
 
     } //Main Method.
+
+    private class SynUser extends AsyncTask<String, Void, String> {
+
+        //Explicit
+        private Context context;
+
+        public SynUser(Context context) {
+            this.context = context;
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            try {
+                OkHttpClient okHttpClient = new OkHttpClient();
+                Request.Builder builder = new Request.Builder();
+                Request request = builder.url(params[0]).build();
+                Response response = okHttpClient.newCall(request).execute();
+                return response.body().string();
+
+
+            } catch (Exception e) {
+                Log.d("13novV2", "e doIn==>" + e.toString());
+                return null;
+            }
+
+
+
+        } //doInBack
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            Log.d("13novV2", "JSON ==>" + s);
+        } // onPost
+
+    }// Syn User
+
 }   //Main Class. นี่คือคลาสหลัก
 
